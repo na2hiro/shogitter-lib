98e90fd72b1b02bba56a82a1214b90d4aa53c142
na2hiro <a@81.la>
2023-09-23 14:16:00 +0900

    Add ai package; Switch to @shogitter org

---
+++ packages/ai/src/bots/randomBot.js
@@ -0,0 +1,33 @@
+import ShogitterCore from "https://esm.sh/@shogitter/core@0.3.1";
+
+async function go(obj) {
+  const shogi = ShogitterCore.ofJkf(obj);
+
+  const moves = shogi.generateMoves();
+  let retries = 1000;
+  while (retries-- > 0) {
+    const move = moves[Math.floor(Math.random() * moves.length)];
+    try {
+      // Test if no exception happens
+      ShogitterCore.ofJkf(obj).runCommand(move);
+
+      await sleep(500); // pretend it's thinking :)
+
+      return move;
+    } catch (e) {
+      // continue
+    }
+  }
+  await sleep(500);
+  console.log("Giving up!!");
+
+  return { type: "resign" };
+}
+
+export default {
+  go,
+}; // TODO: satisfies Bot
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
